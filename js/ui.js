76780bd95b40c758898993cae34d9720da9dbcad
Lyon1994 <765211630@qq.com>
2017-09-16 19:37:11 +0800

    Signed-off-by: Lyon1994 <765211630@qq.com>

--- js/ui.js
+++ js/ui.js
@@ -125,7 +125,7 @@ var navPages = [
 	})),
 	new NavPage("搜索", "img/bt_search.png", new NavPageAdapter({
 		onCreate: function(adapter) {
-			adapter.contentLayout=$('<div id="contentLayout" class="contentLayout"><div><input class="searchInputText" type="search" /></div><span class="textAutoWrap"></span></div>')[0];
+			adapter.contentLayout=$('<div id="contentLayout" class="contentLayout"><div class="searchBar"><input class="searchInputText" type="search" /><span class="searchButton">搜索</span></div><span class="textAutoWrap"></span></div>')[0];
 			adapter.pageBarLayout=$('<div id="pagerBarLayout" class="pagerBar"><div><a href="#" style="padding-left:1rem;padding-right:1rem;">上一页</a><span style="padding-left:1rem;padding-right:1rem;">1/1</span><a href="#" style="padding-left:1rem;padding-right:1rem;">下一页</a></div></div>')[0];
 			adapter.render.appendChild(adapter.contentLayout);
 			adapter.render.appendChild(adapter.pageBarLayout);		
